bb7b611bd9f2e60166fdb27b195e64d8f8ea0e3d
mariano202 <cordobapt202@gmail.com>
2020-11-20 21:24:42 -0500

    publicacion dia 20-11-2020, una nueva navegacion

--- frontend/src/Rutas/App.js
+++ frontend/src/Rutas/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Registro from '../pages/Registro'
-import Menu from '../pages/Menu'
-import Digitar_queja from '../pages/Digitar_queja'
-import Perfil from '../pages/Perfil'
+import Registro from '../pages/Registro';
+import Menu from '../pages/Menu';
+import Digitar_queja from '../pages/Digitar_queja';
+import Perfil from '../pages/Perfil';
 import Inicio from '../pages/Inicio';
+import Menu_admin from '../pages/Menu_admin';
+import Registro_admin from '../pages/Registro_admin';
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={Inicio} />
         <Route path="/menu"component={Menu}/>
+        <Route path="/registro_admin"component={Registro_admin}/>
+        <Route path="/menu_admin"component={Menu_admin}/>
         <Route path="/registro"component={Registro}/>
         <Route path="/digitar_queja"component={Digitar_queja}/>
         <Route path="/perfil" component={Perfil}/>
---
+++ frontend/src/pages/Menu_admin.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav_admin from '../Componentes/Nav_admin';
+import Cuerpo from '../Componentes/Cuerpo';
+
+
+export default function Menu_admin(){
+    return(
+        <>
+<Nav_admin/>
+<Cuerpo/>
+        </>
+    )
+
+}
